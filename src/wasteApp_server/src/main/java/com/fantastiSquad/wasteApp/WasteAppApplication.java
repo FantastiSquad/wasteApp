ad636732a87fd37888f33dff17a04277036dc15e
Philou <pfouque@gmail.com>
2020-05-04 17:29:01 +0200

    initial server source code

---
+++ src/wasteApp_server/src/main/java/com/fantastiSquad/wasteApp/WasteAppApplication.java
@@ -0,0 +1,13 @@
+package com.fantastiSquad.wasteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteAppApplication.class, args);
+	}
+
+}
---
+++ src/wasteApp_server/src/test/java/com/fantastiSquad/wasteApp/WasteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantastiSquad.wasteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
