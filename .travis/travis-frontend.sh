bb7a35ac9594d401a9763722b7344e4eb83899ae
pfouque06 <58525001+pfouque06@users.noreply.github.com>
2020-05-14 01:51:50 +0200

    Update travis-frontend.sh

--- .travis/travis-frontend.sh
+++ .travis/travis-frontend.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-npm install npm@latest @ionic/cli
-
 cd src/wasteApp_client/wasteApp_mob_front
 
-npm install fund && ionic build
+npm install npm@latest @ionic/cli
+
+npm install fund
+
+ionic build
