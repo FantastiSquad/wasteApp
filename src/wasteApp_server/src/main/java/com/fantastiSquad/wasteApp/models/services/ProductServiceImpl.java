918909000ba678b8f311b2211ee46c73d9f65574
Anaëlle Alenda <anaelle.alenda@gmail.com>
2020-06-04 21:54:26 +0200

    fix issue

--- src/wasteApp_server/src/main/java/com/fantastiSquad/wasteApp/models/services/ProductServiceImpl.java
+++ src/wasteApp_server/src/main/java/com/fantastiSquad/wasteApp/models/services/ProductServiceImpl.java
@@ -1,4 +1,4 @@
-spackage com.fantastiSquad.wasteApp.models.services;
+package com.fantastiSquad.wasteApp.models.services;
 
 import com.fantastiSquad.wasteApp.models.entities.Product;
 import com.fantastiSquad.wasteApp.models.repositories.ProductRepository;
