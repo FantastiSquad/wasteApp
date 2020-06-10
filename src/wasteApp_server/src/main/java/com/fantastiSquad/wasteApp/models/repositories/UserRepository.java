e80f9f198ba25609e7857fd12b917f7401b9bde8
Samuel Huguet <samuel.huguet01@gmail.com>
2020-06-10 18:22:06 +0200

    SH_0.2.8.1 Remove JetBrain import not used

--- src/wasteApp_server/src/main/java/com/fantastiSquad/wasteApp/models/repositories/UserRepository.java
+++ src/wasteApp_server/src/main/java/com/fantastiSquad/wasteApp/models/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.fantastiSquad.wasteApp.models.repositories;
 
 import com.fantastiSquad.wasteApp.models.entities.User;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
