369dc95843d1889a52581dda51d4ff81baec40ae
Philou <pfouque@gmail.com>
2020-06-11 00:00:59 +0200

    PF_#1.9_Map v1.2 Add dynamic markers to Leaflet.layerController

--- src/wasteApp_client/wasteApp_mob_front/src/app/map/map.page.ts
+++ src/wasteApp_client/wasteApp_mob_front/src/app/map/map.page.ts
@@ -14,7 +14,7 @@ export class MapPage implements OnInit, OnDestroy {
   public pickupPoints : PickupPoint[];
   //public locality = new FormControl('');
   public locality: string = "";
-  @Input() public localities: string[] = [];
+  @Input() public localities: string[] = ["Nice"];
 
   // Location coordinates : default : Nice / Colline du château / point de vue
   //marker: any;
@@ -26,12 +26,14 @@ export class MapPage implements OnInit, OnDestroy {
   // Leaflet Map
   map: Leaflet.Map;
   zoom: number = 13;
-  overlayMaps: any;
-  marker: Leaflet.marker;
+  layerController: Leaflet.control;
+  layerPickups: Leaflet.layerGroup;
+  layerRecyclers: Leaflet.layerGroup;
   markerGeoloc: Leaflet.icon;
   markerPickup: Leaflet.icon;
   markerRecycler: Leaflet.icon;
-
+  //marker: Leaflet.marker;
+  //overlayMaps: any;
 
   constructor(public pickupPointService: PickupPointService) {
     console.log("MapPage.constructor()");
@@ -40,9 +42,14 @@ export class MapPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log("MapPage.ngOnInit()");
-    this.getPickupPointsByLocalities(this.localities);
+    //this.localityConfirm();
+    // this.getPickupPointsByLocalities(this.localities);
   }
-  ionViewDidEnter() { this.leafletMap(); }
+
+  ionViewDidEnter() { 
+    this.leafletMap();
+    // this.leafletPickupPointsMarkerUpdate();
+  }
 
   ngOnDestroy(): void {
     // Remove map when we have multiple map object
@@ -64,9 +71,10 @@ export class MapPage implements OnInit, OnDestroy {
   async getPickupPointByLocality(locality: string){
     console.log("MapPage.getPickupPointByLocality("+locality+")");
     if (locality) {
+      this.pickupPoints= [];
       this.pickupPoints = await this.pickupPointService.getPickupPointByLocality(locality);
-    } else {
-      this.getAllPickupPoints();
+    // } else {
+    //   this.getAllPickupPoints();
     }
     console.log(this.pickupPoints);
   }
@@ -81,18 +89,25 @@ export class MapPage implements OnInit, OnDestroy {
     console.log(this.pickupPoints);
   }
 
-  getPickupPointsByLocalities(localities: string[]) {
-    console.log("MapPage.getPickupPointsByLocalities("+localities+")");
+  async getPickupPointsByLocalities(localities: string[]) {
+    console.log(">>> MapPage.getPickupPointsByLocalities("+localities+")");
     if (localities.length != 0) {
       this.pickupPoints= [];
 
-      localities.forEach(async locality => {
-        console.log("> locality: "+locality);
+      // localities.forEach(async locality => {
+      //   console.log("> locality: "+locality);
+      //   let points = await this.pickupPointService.getPickupPointByLocality(locality);
+      //   console.log("> points: "+points);
+      //   this.pickupPoints.push(points);
+      //   console.log("> pickupPoints: "+this.pickupPoints);
+      // })
+      for (let locality of localities) {
+        console.log(">>> locality: "+locality);
         let points = await this.pickupPointService.getPickupPointByLocality(locality);
-        console.log("> points: "+points);
+        console.log(">>> points: "+JSON.stringify(points));
         this.pickupPoints.push(points);
-        console.log("> pickupPoints: "+this.pickupPoints);
-      })
+        console.log(">>> pickupPoints: "+this.pickupPoints.toString());
+      }
     }
   }
 
@@ -103,10 +118,13 @@ export class MapPage implements OnInit, OnDestroy {
     this.localities=new Array(this.locality);
     this.localities= this.locality.split(" ");
     console.log("this.localities: "+this.localities);
-    this.getPickupPointsByLocalities(this.localities);
+    // await this.getPickupPointsByLocalities(this.localities);
+    // update pickupPOints LayerGroup
+    this.leafletPickupPointsMarkerUpdate();
   }
 
   leafletMap() {
+    console.log("MapPage.leafletMap()");
     // this.map = Leaflet.map('mapId').setView([this.latitude, this.longitude], this.zoom);
     // Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: 'wasty©2020' }).addTo(this.map);
     
@@ -128,7 +146,8 @@ export class MapPage implements OnInit, OnDestroy {
     this.map = Leaflet.map('mapId', {
       center: [this.latitude, this.longitude], 
       zoom: this.zoom, 
-      layers: [ cdn_base, cdn_voyager, GeoportailFrance ]
+      layers: [ cdn_base, cdn_voyager, GeoportailFrance ],
+      attributionControl: false
     });
 
     // base Maps
@@ -162,30 +181,85 @@ export class MapPage implements OnInit, OnDestroy {
     });
 
     //overlay Maps - mock locations
-    var pickup01 = Leaflet.marker([43.687414, 7.208046], {icon: this.markerPickup}).bindPopup('pickup01'),
-    pickup02    = Leaflet.marker([43.683629, 7.202659], {icon: this.markerPickup}).bindPopup('pickup02'),
-    recycler01    = Leaflet.marker([43.693798, 7.198906], {icon: this.markerRecycler}).bindPopup('recycler01'),
-    recycler02    = Leaflet.marker([43.698536, 7.184249], {icon: this.markerRecycler}).bindPopup('recycler02');
+    // var pickup01 = Leaflet.marker([43.687414, 7.208046], {icon: this.markerPickup}).bindPopup('pickup01'),
+    // pickup02    = Leaflet.marker([43.683629, 7.202659], {icon: this.markerPickup}).bindPopup('pickup02'),
+    // recycler01    = Leaflet.marker([43.693798, 7.198906], {icon: this.markerRecycler}).bindPopup('recycler01'),
+    // recycler02    = Leaflet.marker([43.698536, 7.184249], {icon: this.markerRecycler}).bindPopup('recycler02');
+    
+    this.layerPickups = Leaflet.layerGroup(),
+    this.layerRecyclers = Leaflet.layerGroup();
 
-    var pickups = Leaflet.layerGroup([pickup01, pickup02]),
-    recyclers = Leaflet.layerGroup([recycler01, recycler02]);
+    // var overlayMaps = {
+    //     "Pickups": this.layerPickups,
+    //     "Recyclers": this.layerRecyclers,
+    // };
 
-    var overlayMaps = {
-        "Pickups": pickups,
-        "Recyclers": recyclers,
-    };
-
-    Leaflet.control.layers(baseMaps, overlayMaps).addTo(this.map);
+    // Leaflet.control.layers(baseMaps, overlayMaps).addTo(this.map);
+    this.layerController = Leaflet.control.layers(baseMaps).addTo(this.map);
+    this.leafletPickupPointsMarkerUpdate();
   }
 
-  leafletMarkerUpdate() {
-    
-    this.pickupPoints.forEach(pickup => {
-      switch ( pickup.getDestination) {
-        case "CONTAINER" :
+  leafletPickupPointsMarkerUpdate() {
+    console.log("MapPage.leafletPickupPointsMarkerUpdate()");
+    this.getPickupPointsByLocalities(this.localities);
 
-        case "DECHETTERIE" :
-      }
-    })
+    console.log("> marker update processing ...");
+    var sumLatitude = 0;
+    var sumLongitude = 0;
+    // var markerNumber= this.pickupPoints.length;
+    var markerNumber= 4;
+
+    if (markerNumber != 0) {
+
+      console.log("> overlays cleaning  ...");
+      // remove Layergroups form map or control  ??
+      this.layerController.removeLayer(this.layerPickups);
+      this.layerController.removeLayer(this.layerRecyclers);
+      // or
+      // this.map.removeLayer(this.layerPickups);
+      // this.map.removeLayer(this.layerRecyclers);
+      //or
+      // this.layerPickups.onRemove(this.map);
+      // this.layerRecyclers.onRemove(this.map);
+      // clear Layergroups
+      console.log("> Layergroups cleaning  ...");
+      this.layerPickups.clearLayers(); // clear pickups Layergroup
+      this.layerRecyclers.clearLayers(); // clear recyclers LayerGroup
+      
+      console.log("> pickupPoints scan for new markers  ...");
+      this.pickupPoints.forEach(pickup => {
+        var pickupLat = pickup.getLocation.getGeoLocation.getLatitude; sumLatitude += +pickupLat;
+        var pickupLon = pickup.getLocation.getGeoLocation.getLongitude; sumLongitude += +pickupLon;
+        var locality = pickup.getLocation.getLocality;
+        console.log("> "+locality+" ("+pickupLat+"/"+pickupLon+")");
+        switch ( pickup.getDestination) {
+          case "CONTAINER" :
+            console.log("> CONTAINER");
+            var marker = Leaflet.marker([pickupLat, pickupLon], {icon: this.markerPickup}).bindPopup("Point de collecte - " + locality);
+            this.layerPickups.addLayer(marker);
+            break;
+          case "DECHETTERIE" :
+            console.log("> DECHETTERIE");
+            var marker = Leaflet.marker([pickupLat, pickupLon], {icon: this.markerRecycler}).bindPopup("Déchetterie - " + locality);
+            this.layerRecyclers.addLayer(marker);
+            break;
+        }
+      })
+
+      console.log("> Mock pickupPoints markers  ...");
+      //overlay Maps - mock locations
+      this.layerPickups.addLayer(new Leaflet.marker([43.687414, 7.208046], {icon: this.markerPickup}).bindPopup('pickup01'));
+      this.layerPickups.addLayer(new Leaflet.marker([43.683629, 7.202659], {icon: this.markerPickup}).bindPopup('pickup02'));
+      this.layerRecyclers.addLayer(new Leaflet.marker([43.693798, 7.198906], {icon: this.markerRecycler}).bindPopup('recycler01'));
+      this.layerRecyclers.addLayer(new Leaflet.marker([43.698536, 7.184249], {icon: this.markerRecycler}).bindPopup('recycler02'));
+
+      console.log("> add new markers to layerController  ...");
+      if (this.layerPickups.getLayers().length) this.layerController.addOverlay(this.layerPickups, "Pickups");
+      if (this.layerRecyclers.getLayers().length) this.layerController.addOverlay(this.layerRecyclers, "Recyclers");
+
+      // recenter maps
+      // this.map.panTo(sumLatitude/markerNumber,sumLongitude/markerNumber);
+      // this.map.flyTo(sumLatitude/markerNumber,sumLongitude/markerNumber);
+    }
   }
 }
